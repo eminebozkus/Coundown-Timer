2b282edb5bbc7bedd14724fad1c59e3e106c4dfe
eminebozkus <eminebzks@gmail.com>
2020-11-21 15:23:37 +0300

    countdowntimer

---
+++ script.js
@@ -0,0 +1,28 @@
+const newYears = '01 Jan 2021 00:00'
+const daysEl = document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minsEl = document.getElementById("mins")
+const secondsEl = document.getElementById("seconds")
+
+function countDown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+    const totalSeconds = (newYearsDate - currentDate) / 1000
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 60;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, mins, seconds);
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+
+}
+
+
+
+countDown();
+setInterval(countDown, 1000);
